8c65252ed45154792288c239a234bdc22c635a46
= <=>
2025-07-30 00:28:21 +0500

    all done

---
+++ app.js
@@ -0,0 +1,146 @@
+
+  // import { createClient } from '@supabase/supabase-js'
+  const supabaseUrl = 'https://xwwowgklxvfmcjzdnhaq.supabase.co'
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3d293Z2tseHZmbWNqemRuaGFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NDk4MzUsImV4cCI6MjA2OTEyNTgzNX0.X3gOUHHTcB5I1x9iZy5twify2bjY2Re_YMeR6veIUpQ"
+  const client = supabase.createClient(supabaseUrl, supabaseKey)
+
+  console.log(client);
+
+
+  let todo_input = document.getElementById('todo-input');
+  let add_todo = document.getElementById('add-todo');
+  let todo_list = document.getElementById('todo-list');
+  let clear_all = document.getElementById('clear-all');
+
+
+  window.onload = function () {
+    getshow();
+  add_todo.addEventListener('click',async function () {
+      let todo_text = todo_input.value.trim();
+      
+      if (todo_text === '') {
+            console.error('Todo text cannot be empty');
+            return;
+        
+      }
+      const { error } = await client
+    .from('Todo_list')
+    .insert({ Todotext: todo_text })
+      
+
+      if (error) {
+          console.error('Error adding todo:', error);
+          return;
+      }else{
+          getshow();
+      }
+  
+
+      console.log(`Adding todo: ${todo_text}`);
+      
+      todo_input.value = ''; 
+  })
+
+  }
+
+
+  async function getshow() {
+      const { data, error } = await client
+    .from('Todo_list')
+    .select()
+
+
+    if (error) {
+      console.error('Error fetching todos:', error);
+      return;
+      
+    }else{
+      console.log('Fetched todos:', data);
+
+      todo_list.innerHTML = '';
+      for (let i = 0; i < data.length; i++) {
+          todo_list.innerHTML += `<li>${data[i].id} ${data[i].Todotext} 
+           <div class="todo-buttons">
+        <button onclick="deleteTodo(${data[i].id})"><i class="fa-solid fa-trash"></i></button>
+        <button class="edit-btn" onclick="editTodo(${data[i].id})"><i class="fa-regular fa-pen-to-square"></i></button>
+      </div>
+          </li>
+         
+           `
+           ;  
+          
+          
+      }
+      
+  }
+
+  }
+  // getshow()
+
+
+
+
+  async function deleteTodo(id) {
+  const { error } = await client
+    .from('Todo_list')
+    .delete()
+    .eq('id', id);
+
+  if (error) {
+      console.error('Error deleting todo:', error);
+      return;
+    }else{
+      console.log(`Todo with id ${id} deleted successfully`);
+      getshow(); 
+    }
+      
+  }
+
+
+
+  clear_all.addEventListener('click', async function () {
+      const { error } = await client
+    .from('Todo_list')
+    .delete()
+    .neq('id', 0); 
+
+      if (error) {
+          console.error('Error clearing todos:', error);
+          return;
+      }else{
+          todo_list.innerHTML = '';
+          console.log('All todos cleared successfully');
+      }
+  });
+
+
+  async function editTodo(id) {
+    console.log(`Editing todo with id: ${id}`);
+    let updatetext = todo_input.value.trim();
+    
+    if (updatetext === "") {
+      alert("Please enter a todo text to update.");
+      return;
+    }
+    
+    const { error } = await client
+    .from('Todo_list')
+    .update({ Todotext: updatetext })
+    .eq('id', id);
+
+
+    if (error) {
+      console.log(`Error editing todo with id ${id}:`, error);
+      
+    }
+    else {
+      console.log(`Todo with id ${id} updated successfully`);
+      getshow();
+    }
+
+    todo_input.value = '';
+  }
+
+
+
+
